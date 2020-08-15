156903c2580a3364be866f59bfe24e89db2ded95
max <max_zhen1992@163.com>
2020-08-15 20:29:55 +0800

    master分支修改了第五行

--- src/test/java/com/atguigu/gittest/love.java
+++ src/test/java/com/atguigu/gittest/love.java
@@ -2,6 +2,6 @@ package com.atguigu.gittest;
 
 public class love {
     public static void main(String[] args) {
-        System.out.println("试试git好不好使");
+        System.out.println("master分支：试试git好不好使");
     }
 }
